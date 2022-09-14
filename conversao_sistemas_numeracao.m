579020059d385af0afa5ef2e4bdbdbae19e72120
Gabriel Russo <gabrielrusso@protonmail.com>
2022-09-13 22:24:45 -0400

    Conversões dos tipos primitivos de dado

---
+++ conversao_sistemas_numeracao.m
@@ -0,0 +1,20 @@
+## Conversão de Sistemas de Numeração
+
+disp("BINARIO PARA DECIMAL")
+bin2dec('1001')
+
+disp("DECIMAL PARA OCTAL")
+dec2base(111,8)
+
+#disp("DECIMAL PARA HEXADECIMAL")
+#dev2base(48826, 16)
+
+# Função para converter decimal para binário
+disp("converte.m: DECIMAL PARA BINÁRIO "); converte(37)
+
+# Função para converter decimal para binário
+disp("converte.m: DECIMAL PARA BINÁRIO "); converte(2345)
+
+# Função para converter a parte fracionária do decimal para binário
+disp("converte2.m: FRACIONÁRIO DO DECIMAL PARA BINÁRIO "); converte2(0.1217)
+
